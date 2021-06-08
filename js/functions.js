dee5e272560bd3c0a005cbc85b190e508babe169
Alice <alice.ashford@gmail.com>
2021-06-07 19:12:14 -0500

    portafolio versión para entregar, incompleto

--- js/functions.js
+++ js/functions.js
@@ -2,3 +2,18 @@ function pintar() {
     var element = document.getElementById("menu-drop");
     element.classList.remove("oculto");
 }
+
+
+let slider = document.getElementById("main-slider");
+let header = document.getElementById("header");
+
+
+
+document.addEventListener("scroll", function() {
+
+    if (slider.offsetTop < window.scrollY) {
+        header.style.top = 0;
+        header.classList.add("fixed");
+    }
+
+});
